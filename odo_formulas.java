14b44c4cf1ff10d66f6c1b40f4813528329e3f42
Alexander Bui <72674604+zanbowie138@users.noreply.github.com>
2023-10-12 16:59:18 -0500

    Update odo_formulas.java

--- odo_formulas.java
+++ odo_formulas.java
@@ -2,5 +2,13 @@ float fwd = (R + L)/2
 theta = IMU
 relX = fwd
 relY = strafe
-x = relX * cos(theta) - relY * sin(theta)
-y = relY * cos(theta) + relX * sin(theta)
+
+// Watch if d_theta = 0
+r0 = d_fwd / d_theta
+r1 = d_str / d_theta
+  
+d_relX = r0*sin(d_theta) - r1 * (1-cos(d_theta))
+d_relY = r1*sin(d_theta) + r0 * (1-cos(d_theta))
+  
+x += d_relX * cos(theta) - d_relY * sin(theta)
+y += d_relY * cos(theta) + d_relX * sin(theta)
